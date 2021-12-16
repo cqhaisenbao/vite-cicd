a2c26badc514e7fcf65aa3cc4a481e357c96a368
cqhaisenbao <admin@example.com>
2021-12-16 15:52:07 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="docker" src="./assets/docker.jpeg" />
+  <img style="height: 200px;" alt="docker" src="./assets/docker.jpeg" />
   <h1>hello docker!</h1>
 </template>
 
